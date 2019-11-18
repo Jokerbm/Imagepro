b74604effcc2cb65e556e8262743895ddcc5861f
ryu <ryuchotichan@gmail.com>
2019-11-18 17:08:07 +0700

    BACKEND
    VERSION 2

--- BACKEND/src/main.js
+++ BACKEND/src/main.js
@@ -1,29 +1,30 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import firebase from 'firebase'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./routes/index";
+import * as firebase from "firebase";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
-
-var firebaseConfig = {
+const configOptions = {
   apiKey: "AIzaSyAdMeA2w7XZmIjrJ-xHroLpboaGlC_1Sd4",
-  authDomain: "vue-hotel.firebaseapp.com",
-  databaseURL: "https://vue-hotel.firebaseio.com",
-  projectId: "vue-hotel",
-  storageBucket: "vue-hotel.appspot.com",
-  messagingSenderId: "103876794383",
-  appId: "1:103876794383:web:1290f1395d864713c9ab7a",
-  measurementId: "G-KZV5MV3QB6"
+    authDomain: "vue-hotel.firebaseapp.com",
+    databaseURL: "https://vue-hotel.firebaseio.com",
+    projectId: "vue-hotel",
+    storageBucket: "vue-hotel.appspot.com",
+    messagingSenderId: "103876794383",
+    appId: "1:103876794383:web:1290f1395d864713c9ab7a",
+    measurementId: "G-KZV5MV3QB6"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp(configOptions);
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
