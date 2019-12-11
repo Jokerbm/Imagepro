2713ac513c356e0f453c71bd6bd4682004a2924b
ApichaJA <Junyatanakorn@hotmail.com>
2019-12-11 20:21:40 +0700

    Login-in popup
    Waiting back-end

--- FrontEnd/public/js/main.js
+++ FrontEnd/public/js/main.js
@@ -210,3 +210,15 @@
   }
 
 })(jQuery);
+
+/*---------------------
+   Login-popup
+--------------------- */
+
+var modal = document.getElementById('popup-login');
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
+}
